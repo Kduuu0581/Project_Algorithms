cdfe4d62e9e410743e23d0bc859f955bb91d2a51
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-06-01 16:12:42 -0300

    Requisito 5 - Encontrando números repetidos (Algoritmo de busca)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+from typing import List
+
+
+def find_duplicate(nums: List[int]) -> int:
+    if not nums:
+        return False
+    nums.sort()
+
+    for i in range(len(nums) - 1):
+        if not isinstance(nums[i], int) or nums[i] < 0:
+            return False
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+    return False
